b295f97313b5e34682750c9e1ae0f1a38f519d3e
Luis David Chávez Grimaldo <luiscg404@gmail.com>
2020-11-04 16:18:25 -0600

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite) => {
+    if (!Number(base)) {
+        console.log(`El valor introducido ${base} no es un número`);
+    } else {
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${base}*${i} = ${base*i}\n`;
+        }
+
+        console.log(data);
+    }
+}
+
+let crearArchivo = (base, limite = 10) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${base} no es un número`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${base}*${i} = ${base*i}\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err)
+                reject(err);
+            else
+                resolve(`tabla-${base}.txt`)
+        })
+
+    });
+};
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
